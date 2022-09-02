1231ca7282381f8f59dd79b5f384ab2447d94706
BarbaraZamperete <bahzamp25@gmail.com>
2022-09-02 14:47:46 -0400

    instancia de forma automatica da base de dados

--- src/main/java/com/zamperetta/bookstore/BookstoreApplication.java
+++ src/main/java/com/zamperetta/bookstore/BookstoreApplication.java
@@ -1,13 +1,40 @@
 package com.zamperetta.bookstore;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.zamperetta.bookstore.domain.Categoria;
+import com.zamperetta.bookstore.domain.Livro;
+import com.zamperetta.bookstore.repositories.CategoriaRepository;
+import com.zamperetta.bookstore.repositories.LivroRepository;
+
 @SpringBootApplication
-public class BookstoreApplication {
+public class BookstoreApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private LivroRepository livroReposritory;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática", "Livros de TI");
+		
+		Livro l1 = new Livro(null, "Clean Code", "Robert Martin", "Lorem ipsum", cat1);
+		
+		cat1.getLivros().addAll(Arrays.asList(l1));
+		
+		this.categoriaRepository.saveAll(Arrays.asList(cat1));
+		this.livroReposritory.saveAll(Arrays.asList(l1));
+		
 	}
 
 }
