2e33d0133a6564284084850e86a01fca3786a880
BarbaraZamperete <bahzamp25@gmail.com>
2022-09-05 17:01:24 -0400

    Post para Livros

--- src/main/java/com/zamperetta/bookstore/service/LivroService.java
+++ src/main/java/com/zamperetta/bookstore/service/LivroService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.zamperetta.bookstore.domain.Categoria;
 import com.zamperetta.bookstore.domain.Livro;
 import com.zamperetta.bookstore.repositories.LivroRepository;
 import com.zamperetta.bookstore.service.exceptions.ObjectNotFoundException;
@@ -15,7 +16,7 @@ public class LivroService {
 
 	@Autowired
 	private LivroRepository repository;
-	
+
 	@Autowired
 	private CategoriaService categoriaService;
 
@@ -41,5 +42,12 @@ public class LivroService {
 		newObj.setTitulo(obj.getTitulo());
 		newObj.setNome_autor(obj.getNome_autor());
 		newObj.setTexto(obj.getTexto());
+	}
+
+	public Livro create(Integer id_cat, Livro obj) {
+		obj.setId(null);
+		Categoria cat = categoriaService.findById(id_cat);
+		obj.setCategoria(cat);
+		return repository.save(obj);
 	}
 }
