6fd6c43059a1207385304d3e2df30e22a436d86b
BarbaraZamperete <bahzamp25@gmail.com>
2022-09-05 15:55:06 -0400

    Upate para categoria

--- src/main/java/com/zamperetta/bookstore/service/CategoriaService.java
+++ src/main/java/com/zamperetta/bookstore/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.zamperetta.bookstore.domain.Categoria;
+import com.zamperetta.bookstore.dtos.CategoriaDTO;
 import com.zamperetta.bookstore.repositories.CategoriaRepository;
 import com.zamperetta.bookstore.service.exceptions.ObjectNotFoundException;
 
@@ -29,6 +30,13 @@ public class CategoriaService {
 	
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
+		return repository.save(obj);
+	}
+
+	public Categoria create(Integer id, CategoriaDTO objDto) {
+		Categoria obj = findById(id);
+		obj.setNome(objDto.getNome());
+		obj.setDescricao(objDto.getDescricao());
 		return repository.save(obj);
 	}
 }
